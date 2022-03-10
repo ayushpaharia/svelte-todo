48812997e2bea82a1c89f406f7c65296ae35b0a6
ayushpaharia <ayush.paharia.18@gmail.com>
2022-03-10 12:00:00 +0530

    0.1.0 svelte-todo: init+scss

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+  import "./css/App.scss"
+</script>
+
+<main>
+  <div>
+    <img src={logo} alt="Svelte Logo" />
+    <h1>Svelte Todo</h1>
+    <Counter />
+  </div> 
+</main>
